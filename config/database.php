ae86964a422599f19f178465fb01e3d31390836e
Nde Dilan <nde.dilan@ictuniversity.edu.cm>
2025-06-20 22:42:45 +0100

    Everything working fine, next we add it to portfolio

--- config/database.php
+++ config/database.php
@@ -2,9 +2,15 @@
 class Database {
     private $host = 'localhost';
     private $db_name = 'event_booking_system';
-    private $username = 'dilan';
+    private $username = 'event_booking_system';
     private $password = 't92x.7a!lJZEtGjB';
     private $conn;
+
+    //  private $host = 'sql107.infinityfree.com';
+    // private $db_name = 'if0_39103695_event_booking_system';
+    // private $username = 'if0_39103695';
+    // private $password = 'GRjT7hEFkQ';
+    // private $conn;
 
     public function getConnection() {
         $this->conn = null;
@@ -26,4 +32,14 @@ class Database {
 $database = new Database();
 $pdo = $database->getConnection();
 $db = $database->getConnection();
+
+
+/**
+ * 
+ * User password GRjT7hEFkQ
+ */
+
+
 ?>
+
+
